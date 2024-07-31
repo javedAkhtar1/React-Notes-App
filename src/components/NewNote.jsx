0ad7e4ec526a7e2fc98cb647a9895334d7b0282a
Javed Akhtar <jved004@gmail.com>
2024-07-31 15:23:49 +0530

    added states

--- src/components/NewNote.jsx
+++ src/components/NewNote.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function NewNote() {
+  const [noteHeading, setNoteHeading] = useState("");
+  const [noteContent, setNoteContent] = useState("");
+
   return (
     <>
       <div className="new-note-container">
         <label className="note-heading">Note Heading </label>
-        <input type="text" className="note-heading-input" />
+        <input
+          type="text"
+          className="note-heading-input"
+          value={noteHeading}
+          onChange={(e) => setNoteHeading(e.target.value)}
+        />
 
         <label className="note-content">Note Content </label>
-        <textarea className="note-content-input" name="" id=""></textarea>
+        <textarea
+          className="note-content-input"
+          value={noteContent}
+          onChange={(e) => setNoteContent(e.target.value)}
+        ></textarea>
         <button className="save-btn">Save</button>
       </div>
     </>
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -7,7 +7,10 @@ function Note() {
         <div className='note'>
             <h1>Heading</h1>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste dolore eaque praesentium asperiores veniam suscipit quas doloremque sunt tenetur a. Omnis molestias itaque quis est ex mollitia fuga porro ab, corrupti quaer Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa facilis consequuntur veritatis incidunt dolores corrupti nihil ut quod autem laboriosam? </p>
-            <small  className='note-time'> {date.toLocaleDateString()} {date.toLocaleTimeString()}</small>
+            <div className='note-time-details'>
+            <small  className='note-time'> {date.toLocaleDateString()}</small> 
+            <small> {date.toLocaleTimeString()}</small>
+            </div>
         </div>
     </>
   )
