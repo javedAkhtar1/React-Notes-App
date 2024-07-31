c041851a7345f3cec3e4396fb0e789c25211b46a
Javed Akhtar <jved004@gmail.com>
2024-07-31 15:35:16 +0530

    added reducer

--- src/components/NewNote.jsx
+++ src/components/NewNote.jsx
@@ -1,8 +1,34 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
+
+const ACTIONS = {
+  NOTE_HEADING: "heading",
+  NOTE_CONTENT: "content",
+  NOTE_TIME: "time",
+  NOTE_DATE: "date",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.NOTE_HEADING:
+      return { ...state, noteHeading: action.payload.heading };
+    case ACTIONS.NOTE_CONTENT:
+      return { ...state, noteContent: action.payload.content };
+    case ACTIONS.NOTE_TIME:
+      return { ...state, noteTime: action.payload.time };
+    case ACTIONS.NOTE_DATE:
+      return { ...state, noteDate: action.payload.content };
+    default:
+      return state;
+  }
+}
 
 function NewNote() {
-  const [noteHeading, setNoteHeading] = useState("");
-  const [noteContent, setNoteContent] = useState("");
+  const [state, dispatch] = useReducer(reducer, {
+    noteHeading: "",
+    noteContent: "",
+    noteTime: "",
+    noteDate: "",
+  });
 
   return (
     <>
@@ -11,15 +37,25 @@ function NewNote() {
         <input
           type="text"
           className="note-heading-input"
-          value={noteHeading}
-          onChange={(e) => setNoteHeading(e.target.value)}
+          value={state.noteHeading}
+          onChange={(e) =>
+            dispatch({
+              type: ACTIONS.NOTE_HEADING,
+              payload: { heading: e.target.value },
+            })
+          }
         />
 
         <label className="note-content">Note Content </label>
         <textarea
           className="note-content-input"
-          value={noteContent}
-          onChange={(e) => setNoteContent(e.target.value)}
+          value={state.noteContent}
+          onChange={(e) =>
+            dispatch({
+              type: ACTIONS.NOTE_CONTENT,
+              payload: { content: e.target.value },
+            })
+          }
         ></textarea>
         <button className="save-btn">Save</button>
       </div>
