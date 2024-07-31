1ca97e2d1131b4b71f7f27de83938df447502bde
Javed Akhtar <jved004@gmail.com>
2024-07-31 13:41:10 +0530

    note-card-fix

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 function Note() {
+  const date =  new Date()
   return (
     <>
         <div className='note'>
             <h1>Heading</h1>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste dolore eaque praesentium asperiores veniam suscipit quas doloremque sunt tenetur a. Omnis molestias itaque quis est ex mollitia fuga porro ab, corrupti quaerat! Modi, cum ad.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste dolore eaque praesentium asperiores veniam suscipit quas doloremque sunt tenetur a. Omnis molestias itaque quis est ex mollitia fuga porro ab, corrupti quaer Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa facilis consequuntur veritatis incidunt dolores corrupti nihil ut quod autem laboriosam? </p>
+            <small  className='note-time'> {date.toLocaleDateString()} {date.toLocaleTimeString()}</small>
         </div>
     </>
   )
