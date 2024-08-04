f539fe599060df23b0c2581450d5ddbd86cfa43e
Javed Akhtar <jved004@gmail.com>
2024-08-04 17:05:30 +0530

    stored notes in local storage

--- src/context/NoteContextProvider.jsx
+++ src/context/NoteContextProvider.jsx
@@ -1,25 +1,32 @@
-import { createContext, useState } from "react";
+  import { createContext, useState, useEffect } from "react";
 
-export const NoteContext = createContext();
+  export const NoteContext = createContext();
 
-function NoteContextProvider({ children }) {
-  const [notes, setNotes] = useState([]);
+  function NoteContextProvider({ children }) {
+    const [notes, setNotes] = useState(() => {
+      const storedNotes = localStorage.getItem("notes");
+      return storedNotes ? JSON.parse(storedNotes) : [];
+    });
 
-  const addNote = (note) => {
-    setNotes([...notes, note]);
-  };
+    const addNote = (note) => {
+      setNotes([...notes, note]);
+    };
 
-  const deleteNote = (id) => {
-    setNotes((prevNotes) => prevNotes.filter((note) => note.noteId !== id))
+    const deleteNote = (id) => {
+      setNotes((prevNotes) => prevNotes.filter((note) => note.noteId !== id))
+    }
+
+    useEffect(() => {
+      localStorage.setItem("notes", JSON.stringify(notes));
+    }, [notes]);
+
+    return (
+      <>
+        <NoteContext.Provider value={{ notes, addNote, deleteNote }}>
+          {children}
+        </NoteContext.Provider>
+      </>
+    );
   }
 
-  return (
-    <>
-      <NoteContext.Provider value={{ notes, addNote, deleteNote }}>
-        {children}
-      </NoteContext.Provider>
-    </>
-  );
-}
-
-export default NoteContextProvider;
+  export default NoteContextProvider;
